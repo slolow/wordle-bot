fa7d8b9928b333d3df6f2c65cb10234df72c9aa1
slolow <laszlostarost@gmail.com>
2024-11-03 18:30:10 +0100

    feat: :sparkles: Ignore bots and store playerStatsOfTheDay

--- bot.js
+++ bot.js
@@ -11,13 +11,23 @@ const bot = new TelegramBot(TOKEN, { polling: true });
 bot.on('message', (msg) => {
     const chatId = msg.chat.id.toString();
     const messageText = msg.text;
+    const statsOfTheDay = []
 
     if(chatId === CHAT_ID && messageText.startsWith(WORDLE_MSG_START)) {
         const sender = msg.from
+
+        if (sender.is_bot) {
+            bot.sendMessage(chatId, `${sender.first_name} ist ein Bot! Sein Resultat wird ignoriert`)
+        }
+
         const informationsFromWordleMessage = msg.text.split(" ")
         
         // depending on the users phone settings the gameNumber can be for example '1.223' or '1,223'
         const gameNumber = informationsFromWordleMessage[1].replace(',', '.')
         const numberOfAttempts = Number(informationsFromWordleMessage[2][0])
+
+        const playerStatsOfTheDay = {}
+        playerStatsOfTheDay['player'] = sender.first_name
+        playerStatsOfTheDay['attemps'] = numberOfAttempts
     }
 });
