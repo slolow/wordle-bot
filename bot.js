bcf9f5690803d18ec29472d7829a76cd7116a5f4
slolow <laszlostarost@gmail.com>
2024-11-03 17:40:38 +0100

    feat: :sparkles: Check if message is a wordle message

--- bot.js
+++ bot.js
@@ -1,5 +1,18 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = process.env.TOKEN;
-const bot = new TelegramBot(token, { polling: true });
+const TOKEN = process.env.TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+
+const WORDLE_MSG_START = 'Wordle'
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id.toString();
+    const messageText = msg.text;
+
+    if(chatId === CHAT_ID && messageText.startsWith(WORDLE_MSG_START)) {
+        console.debug('This is a wordle message');
+    }
+});
