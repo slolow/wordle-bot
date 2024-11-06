c9e1cbcba62da98157b48a5f8d04b2abf3078c29
Laszlo Starost <laszlo.starost@barmer.de>
2024-11-06 21:37:20 +0100

    feat: Use restructured data

--- createWinnersOfTheDayMessage.ts
+++ createWinnersOfTheDayMessage.ts
@@ -1,15 +1,17 @@
-import { StatsOfTheDay } from "./data-structure/playerStatsOfTheDay";
+import { PlayerStatsOfTheDay } from "./data-structure/dataTypes";
 
 export const createWinnersOfTheDayMessage = (
-  winnersStatsOfTheDay: StatsOfTheDay,
-) => {
-  const winners = winnersStatsOfTheDay.map((playerStats) => playerStats.player);
+  winnersStatsOfTheDay: PlayerStatsOfTheDay[],
+): string => {
+  const winners: string[] = winnersStatsOfTheDay.map(
+    (playerStats: PlayerStatsOfTheDay) => playerStats.player,
+  );
 
   if (winners.length === 0) {
     return "👎 No one won today. Bunch of looser.";
   }
 
-  const attempts = winnersStatsOfTheDay[0].attempts;
+  const attempts: number = <number>winnersStatsOfTheDay[0].attempts;
 
   return winners.length > 1
     ? `🏆 Today's winners are 🥁... ${winners.slice(0, -1).join(", ")} and ${winners[winners.length - 1]} with 
--- getWinnersStatsOfTheDay.ts
+++ getWinnersStatsOfTheDay.ts
@@ -1,16 +1,21 @@
-import { StatsOfTheDay } from "./data-structure/playerStatsOfTheDay";
+import { PlayerStatsOfTheDay } from "./data-structure/dataTypes";
 
-export const getWinnersStatsOfTheDay = (statsOfTheDay: StatsOfTheDay) => {
-  const possibleWinners: StatsOfTheDay = statsOfTheDay.filter(
-    (playerStats) => playerStats.attempts !== "X",
+export const getWinnersStatsOfTheDay = (
+  statsOfTheDay: PlayerStatsOfTheDay[],
+): PlayerStatsOfTheDay[] => {
+  const possibleWinners: PlayerStatsOfTheDay[] = statsOfTheDay.filter(
+    (playerStats: PlayerStatsOfTheDay) => playerStats.attempts !== "X",
   );
 
   const lowestAttempts = Math.min(
     // @ts-ignore. This ts warning can be ignored since possibleWinners.attempts can not be 'X'
-    ...possibleWinners.map((playerStats) => playerStats.attempts),
+    ...possibleWinners.map(
+      (playerStats: PlayerStatsOfTheDay) => playerStats.attempts,
+    ),
   );
 
   return possibleWinners.filter(
-    (playerStats) => playerStats.attempts === lowestAttempts,
+    (playerStats: PlayerStatsOfTheDay) =>
+      playerStats.attempts === lowestAttempts,
   );
 };
