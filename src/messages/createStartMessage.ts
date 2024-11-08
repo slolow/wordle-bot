30c7497275b730aa163362360a1d2c4a80b0246e
Laszlo Starost <laszlo.starost@barmer.de>
2024-11-08 22:45:37 +0100

    feat: Add nerd stuff and delete info in start message

--- src/messages/createStartMessage.ts
+++ src/messages/createStartMessage.ts
@@ -3,14 +3,22 @@ Welcome to the wordle championship! Together we will find the wordle god!
 My name is Wordletron 3000 and I will keep track of your stats!
   
 The rules are simple:
-  ☝️play wordle every day 
-  📮post your result in this chat
-  🤝be fair: post every result (even if you lost)
+  🎲️ play wordle 
+  📮 post your result in this chat
+  🤝 be fair: post every result (even if you lost)
   
 I will :
-  📢announce the winner of day
-  👑crown the all time leader
-  📊provide you with statistics
+  📢 announce the winner of day
+  👑 crown the all time leader
+  📊 provide you with statistics
 every day at 00:00 local time in Berlin.
 
-Let's go my friends!`;
+Don't be sad if I:
+  🗑️ delete every message that you write except your wordle result
+  
+Let's go my friends!
+
+Nerd stuff:
+  🧑🏻‍💻 check out my brain at https://github.com/slolow/wordle-bot
+  🏃🏻‍ i'm running on an Rapsberry 4 
+`;
